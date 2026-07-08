ce3526eb890383d8013e27cdc6df53ec8efe3d27
maarabhamid-maker <maarabhamid@gmail.com>
2026-07-08 16:48:47 -0400

    NICELY DONE

--- Lost and Found/src/pages/Dashboard.jsx
+++ Lost and Found/src/pages/Dashboard.jsx
@@ -49,7 +49,7 @@ const confirmDelivered = async (matchId) => {
 
   try {
     const res = await fetch(
-      'http://localhost/lostfound/update_match_status.php',
+      'http://maarab.fwh.is/lostfound/update_match_status.php',
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -81,7 +81,7 @@ const hideMatch = async (matchId) => {
 
   try {
     const res = await fetch(
-      `http://localhost/lostfound/hide_match.php?match_id=${matchId}&user_id=${user.id}`
+      `http://maarab.fwh.is/lostfound/hide_match.php?match_id=${matchId}&user_id=${user.id}`
     )
 
     const data = await res.json()
@@ -254,7 +254,7 @@ const handleDelete = async (itemId) => {
                     <div className="flex gap-4">
                       {item.image_url && (
   <img
-    src={`http://localhost/lostfound/${item.image_url}`} // prepend backend URL
+    src={`http://maarab.fwh.ismaarab.fwh.is/lostfound/${item.image_url}`} // prepend backend URL
     alt={item.title}
     className="w-24 h-24 rounded-lg object-cover"
   />
